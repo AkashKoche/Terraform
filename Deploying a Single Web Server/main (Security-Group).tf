352f028923edb4a19d846a22bdf92d14a05829e6
Akash Koche <koche.akash@gmail.com>
2025-01-25 03:13:14 +0530

    Changes Made

--- Deploying a Single Web Server/main (Security-Group).tf
+++ Deploying a Single Web Server/main (Security-Group).tf
@@ -2,19 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "my_instance" {
-  ami           = "ami-0df8c184d5f6ae949"
-  instance_type = "t2.micro"
+  resource "aws_security_group" "my-instance" {
+    name = "terraform-example-instance"
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "HELLO WORLD" >> index.xhtml
-              nohup busybox httpd -f -p 8080 &
-              EOF
-
-  user_data_replace_on_change = true
-
-  tags = {
-    Name = "terraform-example"
+    ingress {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
   }
-}
+ }
